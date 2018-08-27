f0b4bc32c067ea3abd8642ea3a9a245b5bcb14cc
kuanwu.chung@gmail.com <521Chung@gmail.com>
2018-08-28 00:25:54 +0800

    move dir

---
+++ src/src/plugins/vuetify.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VProgressCircular,
+  VBottomNav,
+  VTextField,
+  VInput,
+  VCard,
+  VFooter,
+  VSubheader,
+  VList,
+  VBtn,
+  VBtnToggle,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions
+} from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VProgressCircular,
+    VBottomNav,
+    VTextField,
+    VInput,
+    VCard,
+    VFooter,
+  VSubheader,
+    VList,
+    VBtn,
+    VBtnToggle,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+  },
+})
