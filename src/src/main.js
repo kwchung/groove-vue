50f9a3f1c6a06c90f36df3b6574bcd11dab01fab
chuan <bill0578@gmail.com>
2018-09-02 09:04:06 +0800

    intensity

--- src/src/main.js
+++ src/src/main.js
@@ -7,17 +7,26 @@ import store from './store';
 import './registerServiceWorker';
 import fortawesome from '@fortawesome/fontawesome';
 import faFreeSolid from '@fortawesome/fontawesome-free-solid';
-// import VCharts from 'v-charts'
 import VeLine from 'v-charts/lib/line.common';
-// import VuePickers from "vue-pickers";
+import VeScatter from 'v-charts/lib/scatter.common';
 import VueScrollPicker from 'vue-scroll-picker';
+import * as Firebase from 'firebase/app';
 
 fortawesome.library.add(faFreeSolid);
 
-// Vue.use(VCharts);
-// Vue.use(VuePickers);
+const config = {
+  apiKey: "AIzaSyDSqeEVP7-yjyWhFZOkGih3-9MOBsMVY8s",
+  authDomain: "groove-f2d87.firebaseapp.com",
+  databaseURL: "https://groove-f2d87.firebaseio.com",
+  projectId: "groove-f2d87",
+  storageBucket: "groove-f2d87.appspot.com",
+  messagingSenderId: "1093776659734"
+};
+Firebase.initializeApp(config);
+
 Vue.use(VueScrollPicker);
 Vue.component(VeLine.name, VeLine)
+Vue.component(VeScatter.name, VeScatter)
 
 Vue.config.productionTip = false;
 
--- src/src/views/Intensity.vue
+++ src/src/views/Intensity.vue
@@ -1,23 +1,91 @@
 <template>
-    <v-container>
+    <v-container class="text-xs-center">
         <v-layout>
-            <v-flex>
-                <h1>測試最大肌力</h1>
-                <v-btn
-                    color="success"
-                    @click="$router.push('/purpose')"
-                >選擇健身目標</v-btn>
+            <v-flex xs12>
+                <h1>最大肌力測試</h1>
+                <v-layout row>
+                    <v-flex xs6>
+                        <v-subheader style="padding-top: 20px; font-size: 17px;">負重</v-subheader>
+                    </v-flex>
+                    <v-flex xs6>
+                        <v-text-field
+                            :value="rm"
+                            type="number"
+                            suffix="公斤"
+                        ></v-text-field>
+                    </v-flex>
+                </v-layout>
+                <v-flex xs12>
+                    <v-progress-circular
+                        v-if="show"
+                        :size="70"
+                        :width="7"
+                        color="purple"
+                        indeterminate
+                        ></v-progress-circular>
+                    <ve-scatter v-if="!show" :data="chartData" :legend-visible="false"></ve-scatter>
+                </v-flex>
+                <v-flex xs12>
+                    <v-btn
+                        v-if="show"
+                        color="success"
+                        @click="_finish"
+                    >測試完成</v-btn>
+                    <v-btn
+                        v-if="!show"
+                        color="success"
+                        @click="$router.push('/purpose')"
+                    >選擇健身目標</v-btn>
+                </v-flex>
             </v-flex>
         </v-layout>
     </v-container>
 </template>
 
 <script>
-export default {
+// import * as Firebase from 'firebase/app';
+// import 'firebase/database';
+// const msgRef = Firebase.database().ref("/actions/");
 
-}
+export default {
+  data() {
+    return {
+      show: true,
+      finish: false,
+      rm: 30,
+      actions: [],
+      chartData: {
+        columns: ["重量", "速度", "速度2"],
+        rows: [
+          { 重量: "30", 速度: 0.74, 速度2: 0.7 },
+          { 重量: "40", 速度: 0.65, 速度2: 0.68 },
+          { 重量: "50", 速度: 0.6, 速度2: 0.58 },
+          { 重量: "60", 速度: 0.52, 速度2: 0.5 },
+          { 重量: "70", 速度: 0.43, 速度2: 0.4 }
+        ]
+      }
+    };
+  },
+  mounted() {
+    // for (let i = 0; i < 5; i++) {
+    //   msgRef.push({
+    //     name: `name${i}`,
+    //     type: "text",
+    //   });
+    // }
+    // msgRef.on("value", function(snapshot) {
+    //   const val = snapshot.val();
+    //   vm.actions = val;
+    // });
+  },
+  methods: {
+    _finish() {
+      this.show = false;
+      this.finish = true;
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
